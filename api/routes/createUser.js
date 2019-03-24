8ea96890d44116a71ea8509e460357c51df4332d
Tom <tomhornbuckle1@gmail.com>
2019-03-24 23:56:03 +0000

    first commit

---
+++ api/routes/createUser.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const bodyParser = require('body-parser')
+const mongodb = require('mongodb')
+const mongoURL = 'mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb'
+const saltRounds = 16
+
+mongodb.connect(mongoURL, (err, db) => {
+    if (err) throw err
+    console.log('DATABASE connection achieved!')
+    db.close()
+})
+router.post('/', (req, res, next) => {
+    let password = req.body.password
+    console.log('Username: ' + req.body.username + '\nPassword: ' + password)
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) throw err
+        else {
+            console.log(
+                'Username: ' + req.body.username + '\nPassword: ' + hash
+            )
+            password = hash
+            user = {
+                username: req.body.username,
+                password: password
+            }
+            mongodb.connect(mongoURL, (err, db) => {
+                if (err) throw err
+                const dbo = db.db('mydb')
+                dbo.collection('users').insertOne(user, (err, res) => {
+                    if (err) throw err
+                    console.log('User added to collection')
+                    db.close()
+                })
+            })
+        }
+    })
+})
+
+module.exports = router
---
+++ client/src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import axios from 'axios'
+import './App.css'
+
+const initialState = {
+    apiResponse: '',
+    username: '',
+    password: '',
+    usernameError: '',
+    passwordError: ''
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = initialState
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    validate = () => {
+        let usernameError = ''
+        let passwordError = ''
+        let passwordValidation = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/
+
+        if (!this.state.username) {
+            usernameError = 'Username is blank.'
+        }
+
+        if (passwordValidation.test(this.state.password) !== true) {
+            passwordError = 'Password field is blank.'
+        }
+
+        if (usernameError || passwordError) {
+            this.setState({ usernameError, passwordError })
+            return false
+        }
+        return true
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        const isValid = this.validate()
+        const loginData = {
+            username: this.state.username,
+            password: this.state.password
+        }
+        if (isValid) {
+            axios
+                .post('http://localhost:9000/createUser', loginData)
+                .then(res => console.log(res.data))
+            console.log(loginData)
+            this.setState(initialState)
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <form className="form" onSubmit={this.handleSubmit}>
+                        <label>Create User</label>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Enter a username"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                        <div className="errorMessage">
+                            {this.state.usernameError}
+                        </div>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Enter a Password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                        <div className="errorMessage">
+                            {this.state.passwordError}
+                        </div>
+                        <div className="validationRequirements">
+                            <h3>Password Needs:</h3>
+                            <ul>
+                                <li>At least 8 characters.</li>
+                                <li>At least one captial letter</li>
+                                <li>At least one number</li>
+                            </ul>
+                        </div>
+                        <button type="Submit">Login</button>
+                        <button type="reset">Clear</button>
+                    </form>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default App
